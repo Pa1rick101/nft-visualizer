80270e01c60bc977d7f694000ea577fda25a78de
Patrick <patrick.suehrer@thedac.info>
2022-04-06 18:49:20 +0200

    added NFTCard

--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
-import styled from 'styled-components';
+import { NFTCard } from './components/NFTCard';
+
 
 function App() {
 
-  let nft = { name: "Festival", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"}
+  let nft = [
+    { name: "Festival1", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
+    { name: "Festival2", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
+    { name: "Festival3", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
+    { name: "Festival4", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"}
+  ] 
   
   return (
     <div className="App">
-      <NFTCard nft={nft} />
+      {
+        nft.map((nft, i) =>
+      <NFTCard nft={nft} key= {i} />
+      )
+      }
     </div>
   );
 }
 
-const NFTCard = (props) => {
-  let nft = props.nft
-
-  return(
-    <NftCard>
-      <NftPhoto style={{ backgroundImage: `url(${nft && nft.image})`}}/>
-      <div style={{margin: 5}}>
-        <NftCollectionText> {nft && nft.symbol} </NftCollectionText>
-        <NftName>{nft && nft.name}</NftName>
-        <NftName style={{float: "right"}}>{`x${nft && nft.copies}`}</NftName>
-
-      </div>
-    </NftCard>
-  )
-}
-
-const NftCollectionText = styled.div`
-  font-size: 12px;
-  color: gray;
-`
-
-const NftName = styled.div`
-  font-size: 12px;
-  font-weight: bold;
-  display: inline;
-`
-const NftPhoto = styled.div`
-  display: block;
-  width: 200px;
-  height: 200px;
-  background-position: center center;
-  background-size: cover;
-  border-radius: 10px;
-  margin: auto;
-`
-
-const NftCard = styled.div`
-  width: 200px;
-  height: 250px;
-  margin:  auto;
-  border-radius: 10px;
-  padding: 0px;
-  cursor: pointer;
-  box-shadow: 8px 8px 16px #d9d9d9,
-              -8px -8px 16px #ffffff;
-
-`
 
 export default App;
