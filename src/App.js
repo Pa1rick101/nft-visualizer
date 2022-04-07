c0772cb0099c5799d4ab39346508790002e90b09
Patrick <patrick.suehrer@thedac.info>
2022-04-07 08:26:51 +0200

    finished modal

--- src/App.js
+++ src/App.js
@@ -1,67 +1,56 @@
 import styled from 'styled-components';
-import { NFTCard } from './components/NFTCard';
-
+import { NFTCard, NftPhoto } from './components/NFTCard';
+import { useState } from "react"
+import { NFTModal } from './components/NFTModal';
 
 function App() {
 
-  let nft = [
-    { name: "Festival1", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival2", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival3", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival4", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival1", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival2", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival3", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"},
-    { name: "Festival4", symbol: "MHP", copies: 10, image:"https://via.placeholder.com/150"}
-  ] 
-  
+  const [showModal, setShowModal] = useState(false)
+  const [selectedNft, setSelectedNft] = useState()
+
+  let nfts =
+    [
+      { name: "Festival1", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival2", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival3", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival4", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival5", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival6", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival7", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+      { name: "Festival8", symbol: "MHP", copies: 10, image: "https://via.placeholder.com/150" },
+    ]
+
+  function toggleModal(i) {
+    if (i >= 0) {
+      setSelectedNft(nfts[i])
+    }
+    setShowModal(!showModal)
+  }
+
   return (
     <div className="App">
       <Container>
-        <Title> MHP Festival NFTs</Title>
-        <Subtitle>Gotta Catch 'Em All!</Subtitle>
+        <Title> MHP Festival </Title>
+        <Subtitle> Gotta Catch 'Em Call!</Subtitle>
         <Grid>
-        {
-          nft.map((nft, i) =>
-          <NFTCard nft={nft} key= {i} />
-        )}
+          {
+            nfts.map((nft, i) =>
+              <NFTCard nft={nft} key={i} toggleModal={() => toggleModal(i)} />
+            )
+          }
         </Grid>
       </Container>
-      <NFTModal />
+      {
+        showModal &&
+        <NFTModal
+          nft={selectedNft}
+          toggleModal={() => toggleModal()}
+        />
+      }
 
     </div>
   );
 }
-const NFTModal = (props) => {
-  return (
-    <Modal>
-      <ModalContent>
-
-      </ModalContent>
-    </Modal>
-  )
-}
-const Modal = styled.div`
-  position: fixed;
-  display: flex;
-  align-items: center;
-  z-index: 100;
-  left: 0;
-  top: 0;
-  width: 100%auto;
-  height: 100%;
-  overflow: auto;
-  background-color: rgba(0,0,0,0.5);
-`
-
-const ModalContent = styled.div`
-  position: relative;
-  width: 900px;
-  margin: auto;
-  background-color: white;
-  border-radius: 20px;
-  padding: 20px;
-`
 
 const Title = styled.h1`
   margin: 0;
@@ -78,12 +67,10 @@ const Container = styled.div`
   margin: auto;
   margin-top: 100px;
 `
-
 const Grid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   row-gap: 40px;
 `
-
 
 export default App;
